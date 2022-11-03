8ac305cead9b63019efd744d9d442fa302a77d86
Joe zhao <joezhao1999@gmail.com>
2022-11-03 14:57:49 +0800

    boolean lock in ERC721

--- src/components/lockNFTPage/components/@openzeppelin/contracts/token/ERC721/ERC721.sol
+++ src/components/lockNFTPage/components/@openzeppelin/contracts/token/ERC721/ERC721.sol
@@ -19,10 +19,12 @@ import "../../utils/introspection/ERC165.sol";
 contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     using Address for address;
     using Strings for uint256;
+    // Lock status
+    bool LOCK = true;
 
     // Token name
     string private _name;
-
+    
     // Token symbol
     string private _symbol;
 
@@ -150,10 +152,11 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     function transferFrom(
         address from,
         address to,
-        uint256 tokenId
+        uint256 tokenId,
     ) public virtual override {
         //solhint-disable-next-line max-line-length
         require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
+        require(LOCK == false,"Token is locked! Please unlock it before transferring");
 
         _transfer(from, to, tokenId);
     }
